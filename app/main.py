1098b714dabc889cacf5649fce7bda28962fdc5a
Juan Mosquera <juanpa.mosqueram@gmail.com>
2026-02-09 18:36:05 -0500

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -1,4 +1,5 @@
 import sys
+import os
 
 inBuildCommands = ["echo", "exit", "type"]
 
@@ -19,7 +20,9 @@ def main():
 
 def handle_command(command):
     commandPrefix = command.split()[0]
+    #" ".join une las partes separadas por espacios
     commandComplement = " ".join(command.split()[1:])
+    #switch case altas comparaciones de comandos
     match commandPrefix:  
         case "echo":
             print(commandComplement)
@@ -27,10 +30,22 @@ def handle_command(command):
             if commandComplement in inBuildCommands:
                 print(f"{commandComplement} is a shell builtin")
             else:
-                print(f"{commandComplement}: not found")
+                imported= importedComands(commandComplement)
+                if imported:
+                    print(f"{commandComplement} is {imported}")
+                else:
+                    print(f"{commandComplement}: not found")
         case _:
             print(f"{command}: command not found")
-        
+
+def importedComands(command):
+    path = os.environ["PATH"].split(os.pathsep)
+    for dir in path:
+        fullPath = os.path.join(dir, command)
+        if os.path.isfile(fullPath) and os.access(fullPath, os.X_OK):
+            return fullPath
+    return None
+
 
 
 if __name__ == "__main__":
